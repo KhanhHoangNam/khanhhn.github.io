19931921bb0d5f8ae2d7cfa62b4a4186a3fc7372
KhanhHoangNam <chienbinhbattu9xpro@gmail.com>
2019-10-20 22:22:43 +0700

    update script

--- mongoShell.js
+++ mongoShell.js
@@ -46,7 +46,106 @@
 //     {name: "iphone X1", quantity: 121, size: {width: 2.80, height: 5.56, unit: "inch"}}// Document moi
 // )
 //Xoa mot document trong collection
-db.products.deleteOne({name: "iphone X1"})
+// db.products.deleteOne({name: "iphone X1"})
+//Xoa nhieu documents trong collection
+// db.products.deleteMany({quantity: {$gt: 110}})
+//Gio chung ta co 2 collections: categories va products
+//Quan he giua 2 collection nay: 1-n => category chua "nhieu products"
+// db.categories.insertMany([{
+//     name: 'Electronics',
+//     description: "Đây là danh mục các đồ điện tử",    
+// },{
+//     name: 'Housewares',
+//     description: "Danh mục các đồ gia dụng. VD: điều hoà, tủ lạnh",    
+// },{
+//     name: 'School supplies',
+//     description: "Văn phòng phẩm, đồ dùng học tập",    
+// },{
+//     name: 'Fashion',
+//     description: "Thời trang, quần áo, giày dép",    
+// }
+// ])
+// db.categories.find({}).count()
+// db.products.insertMany([{
+//     name: 'iphone XS',
+//     quantity: 130,
+//     type: 'A',
+//     category: 'Electronics',
+//     size: {width: 2.79, height: 5.65, unit: 'inch'}
+// },{
+//     name: 'track pad',
+//     quantity: 80,
+//     type: 'B',    
+//     category: 'Electronics',
+//     size: {width: 16, height: 9, unit: 'cm'}
+// },{
+//     name: 'Table Lamp',
+//     quantity: 30,
+//     type: 'C',
+//     category: 'Housewares',
+//     size: {width: 5, height: 12, unit: 'inch'}
+// },{
+//     name: 'notebook', 
+//     quantity: 90,
+//     type: 'D',
+//     category: 'School supplies',
+//     size: {width: 9, height: 11, unit: 'cm'}
+// },{
+//     name: 'notebook 2',
+//     quantity: 100,
+//     type: 'D',
+//     category: 'School supplies',
+//     size: {width: 9, height: 11, unit: 'cm'}
+// },{
+//     name: 'Bộ đồ cực cool cotton cho bé từ 1-5 tuổi',
+//     quantity: 12,
+//     type: 'D',
+//     category: 'Fashion',
+//     size: {width: 12, height: 5, unit: 'cm'}
+// },{
+//     name: 'Quần Túi Hộp Kiểu Lính Qh68',
+//     quantity: 5,
+//     type: 'A',
+//     category: 'Fashion',
+//     size: {width: 12, height: 5, unit: 'cm'}
+// },{
+//     name: 'Tủ lạnh 2 ngăn GN-D315PS',
+//     quantity: 34,
+//     type: 'B',
+//     category: 'Housewares',
+//     size: {width: 51, height: 11, unit: 'inch'}
+// },{
+//     name: 'Máy hút bụi LG VC4420NHTPY',
+//     quantity: 34,
+//     type: 'C',
+//     category: 'Housewares',
+//     size: {width: 12, height: 3, unit: 'inch'}
+// },{
+//     name: 'Quần Short Kaki Mài, Loang Các Mầu',
+//     quantity: 13,
+//     type: 'D',
+//     category: 'Fashion',
+//     size: {width: 30, height: 150, unit: 'cm'}
+// },{
+//     name: 'Bộ phần mềm văn phòng Microsoft Office',
+//     quantity: 23,
+//     type: 'D',
+//     category: 'Software',
+//     size: {width: 30, height: 150, unit: 'cm'}
+// }
+// ])
+//Su dung phep join 2 collections de tao ra mot collection co:
+//Thong tin products, kem thong tin CHI TIET tung category
+db.products.aggregate([
+    {
+        $lookup: {
+                from: "categories",
+                localField: "category", //field cua "products"
+                foreignField: "name",  //field cua categories
+                as: "category_doc"
+            }
+        }
+])
 
 
 
